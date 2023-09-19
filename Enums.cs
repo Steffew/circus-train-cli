71ca9863f33040acd46beb08b65b57c80d094778
Steffew <xgenyofficial@gmail.com>
2023-09-19 15:39:15 +0200

    Add Enums Class

---
+++ Enums.cs
@@ -0,0 +1,15 @@
+﻿internal class Enums
+{
+    public enum Size
+    {
+        Small = 1,
+        Medium = 3,
+        Large = 5,
+    }
+
+    public enum Type
+    {
+        Herbivore,
+        Carnivore,
+    }
+}
