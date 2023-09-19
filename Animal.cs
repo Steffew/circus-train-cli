fda81abe333516e89f58c7a077b46eeb4e11e7af
Steffew <xgenyofficial@gmail.com>
2023-09-19 15:46:00 +0200

    Add Animal Class

---
+++ Animal.cs
@@ -0,0 +1,11 @@
+﻿internal class Animal
+{
+    private Enums.Type type;
+    private Enums.Size size;
+
+    public Animal(Enums.Type type, Enums.Size size)
+    {
+        this.type = type;
+        this.size = size;
+    }
+}
