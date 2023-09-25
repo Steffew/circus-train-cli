1b7919b944ee96f3be7625389c1b719936d75c32
Steffew <xgenyofficial@gmail.com>
2023-09-25 09:56:44 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
-﻿List<Animal> animals = new List<Animal>()
+﻿Train train = new Train();
+
+List<Animal> animals = new List<Animal>()
 {
     new Animal(Enums.Type.Herbivore, Enums.Size.Small),
     new Animal(Enums.Type.Herbivore, Enums.Size.Large),
-    new Animal(Enums.Type.Herbivore, Enums.Size.Small),
+    new Animal(Enums.Type.Carnivore, Enums.Size.Small),
     new Animal(Enums.Type.Herbivore, Enums.Size.Medium),
     new Animal(Enums.Type.Herbivore, Enums.Size.Large),
+
 };
 
+train.SortAnimals(animals);
+train.AssignAnimals(animals);
+train.PrintAnimals(animals);
