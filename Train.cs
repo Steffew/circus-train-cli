4b83867b6fc2322ed32c3c8494c28d73a52dd27c
Steffew <xgenyofficial@gmail.com>
2023-09-25 09:56:40 +0200

    Add AssignAnimals method

--- Train.cs
+++ Train.cs
@@ -20,4 +20,40 @@
     {
         animals = animals.OrderBy(animal => animal.Size).ThenBy(animal => animal.Type).ToList();
     }
+
+    public void AssignAnimals(List<Animal> animals)
+    {
+        foreach (var animal in animals)
+        {
+            bool isAssigned = false;
+
+            var suitableWagon = Wagons
+                .OrderBy(w => 10 - w.Size)
+                .FirstOrDefault(w => w.CanAddAnimal(animal));
+
+            if (suitableWagon != null)
+            {
+                suitableWagon.AddAnimal(animal);
+                isAssigned = true;
+            }
+
+            if (!isAssigned)
+            {
+                var newWagon = AddWagon();
+                newWagon.AddAnimal(animal);
+            }
+        }
+    }
+
+    public void PrintAnimals(List<Animal> animals)
+    {
+        foreach (Wagon wagon in Wagons)
+        {
+            Console.WriteLine($"Wagon ID: {wagon.Id} - Total size: {wagon.Size}");
+            foreach (Animal animal in wagon.Animals)
+            {
+                Console.WriteLine($"  Animal: {animal.Type} - {animal.Size}");
+            }
+        }
+    }
 }
